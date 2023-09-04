bbea94f8e191c3c0e5d2412dbb79b21bb004c2e0
Zhen Zhong <auroradysis@gmail.com>
2023-09-04 05:59:12 +0100

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -15,7 +15,7 @@ using Float128 = std::tuple<boost::multiprecision::float128, boost::multiprecisi
 
 #ifdef BIGFLOAT
 using BigFloat = std::tuple<boost::multiprecision::mpfr_float_50, boost::multiprecision::mpc_complex_50>;
-const boost::multiprecision::mpfr_float_50 ERROR_LIMIT{"1e-45"};
+const boost::multiprecision::mpfr_float_50 BIGFLOAT_ERROR_LIMIT{"1e-45"};
 #endif
 
 TEMPLATE_TEST_CASE("Forward Function", "[forward]", BigFloat) {
@@ -41,6 +41,13 @@ TEMPLATE_TEST_CASE("Forward Function", "[forward]", BigFloat) {
   radial_integral[0] = boost::lexical_cast<Real>("1.449918724448707783003361604770409888333642555992048976602177112826476986925901542332371494798805");
   radial_integral[1] = boost::lexical_cast<Real>("1.288507079276017729837985995399653209220514835403767425334993622951859438005362259829818057986322");
   radial_integral[2] = boost::lexical_cast<Real>("1050.230729556838727739610680482910331549495652276193749581856090200250574410573699314114422714724239");
+
+  // {1.449918724448707783003361604770409888333642555992048976602177112826476986925901542332371494800308`74.93029591402417, 9.034760480706797702786289961054367423398533894470186848502176405935371833747610240436842225095523`72.26327202415743, 0.684953649593118508702107588106324458927421602634607435896061608959165805326668612330938804832442`72.54800818746092}
+  std::array<Real, 3> angular_integral;
+  angular_integral[0] = boost::lexical_cast<Real>("1.449918724448707783003361604770409888333642555992048976602177112826476986925901542332371494800308");
+  angular_integral[1] = boost::lexical_cast<Real>("9.034760480706797702786289961054367423398533894470186848502176405935371833747610240436842225095523");
+  angular_integral[2] = boost::lexical_cast<Real>("0.684953649593118508702107588106324458927421602634607435896061608959165805326668612330938804832442");
+
   // is big float
 #ifdef FLOAT128
   if constexpr (std::is_same_v<TestType, FLOAT128>) {
@@ -49,41 +56,14 @@ TEMPLATE_TEST_CASE("Forward Function", "[forward]", BigFloat) {
 #endif
 #ifdef BIGFLOAT
   if constexpr (std::is_same_v<TestType, BigFloat>) {
-    CHECK(abs(forward.radial_integrals[0] - radial_integral[0]) < ERROR_LIMIT);
-    CHECK(abs(forward.radial_integrals[1] - radial_integral[1]) < ERROR_LIMIT);
-    CHECK(abs(forward.radial_integrals[2] - radial_integral[2]) < ERROR_LIMIT);
-    CHECK(abs(forward.theta_f - theta_o) < ERROR_LIMIT);
-    CHECK(abs(forward.phi_f - phi_o) < ERROR_LIMIT);
+    CHECK(abs(forward.radial_integrals[0] - radial_integral[0]) < BIGFLOAT_ERROR_LIMIT);
+    CHECK(abs(forward.radial_integrals[1] - radial_integral[1]) < BIGFLOAT_ERROR_LIMIT);
+    CHECK(abs(forward.radial_integrals[2] - radial_integral[2]) < BIGFLOAT_ERROR_LIMIT);
+    CHECK(abs(forward.angular_integrals[0] - angular_integral[0]) < BIGFLOAT_ERROR_LIMIT);
+    CHECK(abs(forward.angular_integrals[1] - angular_integral[1]) < BIGFLOAT_ERROR_LIMIT);
+    CHECK(abs(forward.angular_integrals[2] - angular_integral[2]) < BIGFLOAT_ERROR_LIMIT);
+    CHECK(abs(forward.theta_f - theta_o) < BIGFLOAT_ERROR_LIMIT);
+    CHECK(abs(forward.phi_f - phi_o) < BIGFLOAT_ERROR_LIMIT);
   }
 #endif
-
-//    ForwardRayTracing forward();
-//    SECTION("lamqv2") {
-//        Real a = RCONST(0.9);
-//        Real rc = RCONST(3);
-//        Real d = RCONST(-1);
-//        auto res = lamqv2(a, rc, d);
-//        std::array<Real, 2> expected = {-RCONST(9.0) / RCONST(5.0),
-//                                        RCONST(-1.0) + RCONST(3.0) * std::sqrt(RCONST(3.0))};
-//        for (int i = 0; i < 2; ++i) {
-//            REQUIRE_THAT(res[i],
-//                         Catch::Matchers::WithinRel(expected[i], 1e-15) ||
-//                         Catch::Matchers::WithinAbs(expected[i], 1e-15));
-//        }
-//    }
-//
-//    SECTION("lametav2") {
-//        Real a = RCONST(0.9);
-//        Real rc = RCONST(3);
-//        Real d = RCONST(-1);
-//        auto res = lametav2(a, rc, d);
-//        std::array<Real, 2> expected = {-RCONST(9.0) / RCONST(5.0),
-//                                        RCONST(28.0) - RCONST(6.0) * std::sqrt(RCONST(3.0))};
-//        for (int i = 0; i < 2; ++i) {
-//            REQUIRE_THAT(res[i],
-//                         Catch::Matchers::WithinRel(expected[i], 1e-15) ||
-//                         Catch::Matchers::WithinAbs(expected[i], 1e-15));
-//        }
-//    }
 }
-
