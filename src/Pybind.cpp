f087d458a2fb8a7e913539f5b42f9804734ecbce
AuroraDysis <auroradysis@gmail.com>
2023-09-04 17:33:04 +0100

    dev

--- src/Pybind.cpp
+++ src/Pybind.cpp
@@ -10,6 +10,7 @@ template <typename Real, typename Complex>
 void define_forward_ray_tracing(pybind11::module_ &mod, const char *name) {
   using RayTracing = ForwardRayTracing<Real, Complex>;
   py::class_<RayTracing>(mod, name)
+      .def(py::init<>())
       .def_readonly("a", &RayTracing::a)
       .def_readonly("rp", &RayTracing::rp)
       .def_readonly("rm", &RayTracing::rm)
@@ -28,18 +29,32 @@ void define_forward_ray_tracing(pybind11::module_ &mod, const char *name) {
       .def_readonly("theta_f", &RayTracing::theta_f)
       .def_readonly("phi_f", &RayTracing::phi_f)
       .def_readonly("m", &RayTracing::m)
-      .def_readonly("n_half", &RayTracing::n_half);
+      .def_readonly("n_half", &RayTracing::n_half)
+      .def_readonly("ray_status", &RayTracing::ray_status)
+      .def("calc_ray_by_lambda_q", &RayTracing::calc_ray_by_lambda_q)
+      .def("calc_ray_by_rc_d", &RayTracing::calc_ray_by_rc_d);
 }
 
 PYBIND11_MODULE(py_forward_ray_tracing, mod) {
+  py::enum_<RayStatus>(mod, "RayStatus")
+      .value("NORMAL", RayStatus::NORMAL)
+      .value("CONFINED", RayStatus::CONFINED)
+      .value("ETA_OUT_OF_RANGE", RayStatus::ETA_OUT_OF_RANGE)
+      .value("THETA_OUT_OF_RANGE", RayStatus::THETA_OUT_OF_RANGE)
+      .value("UNKOWN_ERROR", RayStatus::UNKOWN_ERROR)
+      .export_values();
+
+  py::enum_<Sign>(mod, "Sign")
+      .value("POSITIVE", Sign::POSITIVE)
+      .value("NEGATIVE", Sign::NEGATIVE)
+      .export_values();
+
   define_forward_ray_tracing<double, std::complex<double>>(mod, "ForwardRayTracingFloat64");
   define_forward_ray_tracing<long double, std::complex<long double>>(mod, "ForwardRayTracingLongDouble");
 #ifdef FLOAT128
-  define_forward_ray_tracing<boost::multiprecision::float128, boost::multiprecision::complex128>(mod,
-                                                                                                 "ForwardRayTracingFloat128");
+  // define_forward_ray_tracing<boost::multiprecision::float128, boost::multiprecision::complex128>(mod, "ForwardRayTracingFloat128");
 #endif
 #ifdef BIGFLOAT
-  py::class_<ForwardRayTracing<boost::multiprecision::mpfr_float_50, boost::multiprecision::mpc_complex_50>>(mod,
-                                                                                                             "ForwardRayTracingBigFloat50");
+  // define_forward_ray_tracing<BigFloat, BigComplex>(mod, "ForwardRayTracingBigFloat");
 #endif
 }
