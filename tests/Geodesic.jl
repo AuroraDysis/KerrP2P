9e6acd7ab93bb1599a8eb4107d688831de0d6391
Zhen Zhong <auroradysis@gmail.com>
2023-09-09 02:45:07 +0100

    Update Geodesic.jl

--- tests/Geodesic.jl
+++ tests/Geodesic.jl
@@ -13,10 +13,10 @@ end
 
 function stop_condition_r(u, t, integrator)
     @unpack rh, ro = integrator.p
-    u[2] <= 1.01 * rh || u[2] >= ro
+    u[1] <= 1.01 * rh || u[1] >= ro
 end
 
-function calc_ray(M::T, a::T, pos::Array{T, 1}, λ::T, η::T, ν_r::Int, ν_θ::Int, ro::T, atol::T, rtol::T) where T <: AbstractFloat
+function calc_ray(M::T, a::T, pos::Array{T, 1}, λ::T, η::T, ν_r::Int, ν_θ::Int, ro::T, tend::T, atol::T, rtol::T) where T <: AbstractFloat
     t, r, θ, ϕ = pos
 
     sinθ, cosθ = sincos(θ)
@@ -49,36 +49,44 @@ function calc_ray(M::T, a::T, pos::Array{T, 1}, λ::T, η::T, ν_r::Int, ν_θ::
     rh = M + sqrt(M^2 - a^2)
     cache = KerrCache{T}(M, a, λ, rh, ro)
 
-    u0 = [t, r, θ, ϕ, pr, pθ]
-    prob = ODEProblem(eom_back!, u0, (zero(T), typemax(T)), cache)
+    u0 = [r, θ, ϕ, pr, pθ]
+    prob = ODEProblem(eom_back!, u0, (zero(T), tend), cache)
     cb = ContinuousCallback(stop_condition_r, igt -> terminate!(igt))
     # only save the last step
-    sol = solve(prob, Vern8(), reltol=1e-10, abstol=1e-10, save_start = false, save_everystep = false, callback=cb)
+    method = if atol < 1e-12
+        Vern9()
+    else
+        Vern8()
+    end
+    sol = solve(prob, method, abstol=atol, reltol=rtol, save_start = false, saveat = [tend], callback=cb)
 
-    print(sol[end])
+    println(sol.t[end])
+    println(sol[end])
 end
 
-function eom_back!(du::Array{T, 1}, u::Array{T, 1}, p::KerrCache{T}, t::T) where T <: AbstractFloat
+function eom_back!(du::Array{T, 1}, u::Array{T, 1}, p::KerrCache{T}, τ::T) where T <: AbstractFloat
     @unpack M, a, λ = p
-    t, r, θ, ϕ, pr, pθ = u
+    r, θ, ϕ, pr, pθ = u
 
+    dtdtau = 4 * r .* (-λ .* a + a .^ 2 + r .^ 2) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2)) + 1
     @inbounds begin
-        du[1] = 4 * r .* (-λ .* a + a .^ 2 + r .^ 2) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2)) + 1
-        du[2] = pr .* (a .^ 2 + r .* (r - 2)) ./ (a .^ 2 .* cos(θ) .^ 2 + r .^ 2)
-        du[3] = pθ ./ (a .^ 2 .* cos(θ) .^ 2 + r .^ 2)
-        du[4] = (2 * λ .* (a .^ 2 + r .* (r - 2)) .* csc(θ) .^ 2 + 2 * a .* (-λ .* a + 2 * r)) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2))
-        du[5] = (-a .^ 2 .* pr .^ 2 .* (r - 1) .* cos(θ) .^ 2 + r .* (pr .^ 2 .* (a .^ 2 - r) + pθ .^ 2)) ./ (a .^ 2 .* cos(θ) .^ 2 + r .^ 2) .^ 2 + (-6 * λ .^ 2 .* a .^ 4 .* r + 4 * λ .^ 2 .* r .* (a .^ 2 + r .* (r - 2)) .^ 2 .* csc(θ) .^ 2 + 12 * λ .* a .^ 2 .* r .^ 2 .* (λ + a) + 2 * a .^ 4 .* (-λ + a) .^ 2 + 2 * a .^ 2 .* (-λ .^ 2 .* a .^ 2 .* r + a .^ 2 .* (-λ + a) .^ 2 + 2 * a .* r .^ 2 .* (λ + a) + r .^ 4 - 4 * r .^ 3) .* cos(2 * θ) - 6 * a .* r .^ 4 .* (-4 * λ + a) + 8 * a .* r .^ 3 .* (-λ .^ 2 .* a - 4 * λ + a) - 4 * r .^ 6) ./ ((a .^ 2 + r .* (r - 2)) .^ 2 .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2) .^ 2)
-        du[6] = λ .^ 2 .* (4 * a .^ 4 .* r .* sin(2 * θ) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2) .^ 2) + cot(θ) .* csc(θ) .^ 2) ./ (a .^ 2 + r .^ 2) - 2 * a .^ 2 .* (4 * λ .* a .* r + a .^ 4 .* pr .^ 2 + a .^ 2 .* (pθ .^ 2 + 2 * r .* (pr .^ 2 .* (r - 2) - 1)) + r .* (pθ .^ 2 .* (r - 2) + r .* (pr .^ 2 .* (r - 2) .^ 2 - 2 * r))) .* sin(2 * θ) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2) .^ 2)
+        du[1] = pr .* (a .^ 2 + r .* (r - 2)) ./ (a .^ 2 .* cos(θ) .^ 2 + r .^ 2)
+        du[2] = pθ ./ (a .^ 2 .* cos(θ) .^ 2 + r .^ 2)
+        du[3] = (2 * λ .* (a .^ 2 + r .* (r - 2)) .* csc(θ) .^ 2 + 2 * a .* (-λ .* a + 2 * r)) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2))
+        du[4] = (-a .^ 2 .* pr .^ 2 .* (r - 1) .* cos(θ) .^ 2 + r .* (pr .^ 2 .* (a .^ 2 - r) + pθ .^ 2)) ./ (a .^ 2 .* cos(θ) .^ 2 + r .^ 2) .^ 2 + (-6 * λ .^ 2 .* a .^ 4 .* r + 4 * λ .^ 2 .* r .* (a .^ 2 + r .* (r - 2)) .^ 2 .* csc(θ) .^ 2 + 12 * λ .* a .^ 2 .* r .^ 2 .* (λ + a) + 2 * a .^ 4 .* (-λ + a) .^ 2 + 2 * a .^ 2 .* (-λ .^ 2 .* a .^ 2 .* r + a .^ 2 .* (-λ + a) .^ 2 + 2 * a .* r .^ 2 .* (λ + a) + r .^ 4 - 4 * r .^ 3) .* cos(2 * θ) - 6 * a .* r .^ 4 .* (-4 * λ + a) + 8 * a .* r .^ 3 .* (-λ .^ 2 .* a - 4 * λ + a) - 4 * r .^ 6) ./ ((a .^ 2 + r .* (r - 2)) .^ 2 .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2) .^ 2)
+        du[5] = λ .^ 2 .* (4 * a .^ 4 .* r .* sin(2 * θ) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2) .^ 2) + cot(θ) .* csc(θ) .^ 2) ./ (a .^ 2 + r .^ 2) - 2 * a .^ 2 .* (4 * λ .* a .* r + a .^ 4 .* pr .^ 2 + a .^ 2 .* (pθ .^ 2 + 2 * r .* (pr .^ 2 .* (r - 2) - 1)) + r .* (pθ .^ 2 .* (r - 2) + r .* (pr .^ 2 .* (r - 2) .^ 2 - 2 * r))) .* sin(2 * θ) ./ ((a .^ 2 + r .* (r - 2)) .* (a .^ 2 .* cos(2 * θ) + a .^ 2 + 2 * r .^ 2) .^ 2)
     end
+    du ./= dtdtau
 end
 
 function main()
     λ = -0.7511316141196980351821846354387491739005901369953212373679122913636
     η = 26.5724289697094692725198762793446996667830541429568972667550981404403
     ro = 1000.0
+    tend = 1050.66909989257832358518002933929837920320920210187
     atol = 1e-10
     rtol = 1e-10
-    calc_ray(1.0, 0.8, [0.0, 10.0, pi / 2, 0.0], λ, η, -1, -1, ro, atol, rtol)
+    calc_ray(1.0, 0.8, [0.0, 10.0, pi / 2, 0.0], λ, η, -1, -1, ro, tend, atol, rtol)
 end
 
 main()
